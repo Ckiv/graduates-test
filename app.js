358c26fe2e53326f6902c6b4106ee9b4897ac29b
Ckiv <alexandrshahok@gmail.com>
2020-03-10 16:38:00 +0200

    почти готова полноценная запись в бд, проблема с неполными входными данными

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-
-
+app.set("view engine", "hbs");
+app.use(express.static('public'));
 
 
 const pool = mysql.createPool({
@@ -15,21 +15,69 @@ const pool = mysql.createPool({
     port: 3306
 });
 
-app.set("view engine", "hbs");
-app.use(express.static('public'));
 
-// запись в БД из Excel
-var parseXlsx = require('excel').default;
-parseXlsx('public/grad.xlsx').then((data) => {
 
-    for (var i=0; i<=data[0].length; i++){
-        pool.query("INSERT INTO training (type_training) VALUES (?)", [data[i][1]], function(err, data) {
+// запись из Excel в переменную
+var parser = new (require('simple-excel-to-json').XlsParser)();
+var doc = parser.parseXls2Json('./public/grad-db.xlsx');
+//print the data of the first sheet
+var data = doc[0];
+let i=0;
+while (i<=data.length){
+    console.log("ид диплома: " + i + " номер диплома: " + data[i].diploma);
+    i++;
+}
+
+/*
+// запись в БД
+async function setDataGraduate() {
+
+    const specialty = await pool.query('SELECT * FROM specialty');
+    const faculty = await pool.query('SELECT * FROM faculty');
+    const user = await pool.query('SELECT * FROM userr');
+    const training = await pool.query('SELECT * FROM training');
+
+    let idSpecialty;
+    let idFaculty;
+    let idUser=2;
+    let idTraining;
+
+    for (let i=0; i<=1000; i++){
+        //написать условия для определения id для записи
+        for (let tempSpec=0; tempSpec<specialty[0].length; tempSpec++){
+            if (data[i].specialty === specialty[0][tempSpec].title_specialty){
+                idSpecialty = specialty[0][tempSpec].id;
+            }
+        }
+
+        for (let tempFacult=0; tempFacult<faculty[0].length; tempFacult++){
+            if (data[i].faculty === faculty[0][tempFacult].title_faculty){
+                idFaculty = faculty[0][tempFacult].id;
+            }
+        }
+
+        for (let tempTrain=0; tempTrain<training[0].length; tempTrain++){
+            if (data[i].training === training[0][tempTrain].type_training){
+                idTraining = training[0][tempTrain].id;
+            }
+        }
+
+        pool.query("INSERT INTO graduate (id_specialty, id_faculty, id_user, id_training, groupp, year, diploma, name, lastname, patronymic) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [idSpecialty, idFaculty, idUser, idTraining, data[i].group, data[i].year, data[i].diploma, data[i].name, data[i].lastname, data[i].patronymic],  function(err, data) {
             if(err) return console.log(err);
+            console.log("выполнено");
         });
 
-        console.log(data)
+        idSpecialty=0;
+        idFaculty=0;
+        idTraining=0;
+
     }
-});
+    console.log("успешная запись в бд");
+}
+
+setDataGraduate();
+*/
+
 
 
 // получение списка выпускников
